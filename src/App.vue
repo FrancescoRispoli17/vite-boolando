8bb09ae143899a97d4aafbf4e391e60687dc397b
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-06-04 05:09:43 +0200

    [UPD] added header (not complete)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script>
+import Header from './components/Header.vue';
 
+export default{
+    name:'MyApp',
+    components:{
+        Header,
+    },
+    data(){
+        return{
+            title:'aaaaaa',
+        }
+    },
+}
 </script>
 
 <template>
-  
+<Header/>
+
 </template>
 
-<style scoped land="scss">
+<style scoped>
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<script>
+    export default{
+        name:'Header',
+        data(){
+            return{
+                MenuFilter:[
+                    {
+                        name:'Uomo',
+                    },
+                    {
+                        name:'Donna',
+                    },
+                    {
+                        name:'Bambino',
+                    },
+                ],
+                MenuIcons:[
+                ]
+            }
+        },
+    };
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col" >
+                    <ul>
+                        <li v-for="filter in MenuFilter">{{ filter.name }}</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <img src="../assets/img/boolean-logo.png" alt="logo">
+                </div>
+                <div class="col">
+                    
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables.scss' as *;
+
+header{
+    background-color: $header-color;
+    padding: 1.5rem 0;
+    color: white;
+    font-size: 20px;
+
+    img{
+        width: 150px;
+        margin: 0 10rem;
+    }
+}
+</style>
